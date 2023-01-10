457668dfe5e46257e2ed9c56468453d29adc4f98
Büşra Gülen <65137550+busragulen@users.noreply.github.com>
2023-01-10 13:29:51 +0300

    Update float_to_ascii_func.c

--- float_to_ascii_func.c
+++ float_to_ascii_func.c
@@ -1,19 +1,16 @@
-float value; 
-int data_no;
-char queue[20][4];
+char* fto(float value[100], char queue[4]){ // fto = float to ascii
+    
+    for (int i=0; i<100; i++){
+    value[i]+=33.33f;
 
-char* float_to_ascii(float value, int data_no){
-    value+=33.33;
-    
-    int decimal= (int)value; // 33.00
-    int tenth= value*100 - decimal*100; // 00.33
-    
-   
-    queue[data_no][0]=(char)decimal;
-    queue[data_no][1]='.';
-    queue[data_no][2]=(char)tenth;
-    queue[data_no][3]='\0';
-       
-    
-    return queue[data_no];
+    int decimal= (int)value[i]; // 33.00
+    int tenth= value[i]*100 - decimal*100; // 00.33
+
+    queue[0]=(char)decimal;
+    queue[1]='.';
+    queue[2]=(char)tenth;
+    queue[3]='\0';
+
+    return queue;
+    }
 }
